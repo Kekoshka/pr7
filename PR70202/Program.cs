5aa5ba62dd63d2171cd5f04c2a1c4ce6b08986d4
Исаков Александр Сергеевич <isakov.as@prg-it.ru>
2025-12-15 22:25:38 +0500

    Должно воркать

--- PR70202/Program.cs
+++ PR70202/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+Cookie token = SingIn("user", "user");
+string content = GetContent(token);
+ParsingHtml(content);
+
+Console.Read();
+
+void ParsingHtml(string htmlCode)
+{
+    var html = new HtmlDocument();
+    html.LoadHtml(htmlCode);
+    var document = html.DocumentNode;
+    var divsNews = document.Descendants(0).Where(n => n.HasClass("news"));
+    foreach(HtmlNode divNews in divsNews)
+    {
+        var src = divNews.ChildNodes[1].GetAttributeValue("src", "none");
+        var name = divNews.ChildNodes[3].InnerText;
+        var description = divNews.ChildNodes[5].InnerText;
+        Console.WriteLine($"{name}\nИзображение: {src}\nОписание: {description}\n");
+    }
+}
+string GetContent(Cookie token)
+{
+    string url = "";
+    Debug.WriteLine($"Выполняем запрос {url}");
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    request.CookieContainer = new CookieContainer();
+    request.CookieContainer.Add(token);
+
+    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+    Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+    string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+    return content;
+}
+Cookie SingIn(string login, string password)
+{
+    Cookie token = null;
+    string url = "";
+
+    Debug.WriteLine($"Выполняем запрос: {url}");
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "POST";
+    request.ContentType = "application/x-www-form-urlencoded";
+    request.CookieContainer = new();
+    byte[] data = Encoding.ASCII.GetBytes($"login={login}&password={password}");
+    request.ContentLength = data.Length;
+    using Stream stream = request.GetRequestStream();
+    stream.Write(data, 0, data.Length);
+
+    using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+    Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+    string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+    Console.WriteLine(responseFromServer);
+    token = response.Cookies["token"];
+    return token;
+
+}
